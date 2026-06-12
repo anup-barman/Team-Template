7e8dd4c7aa46fc9d69a45d3a486598ded7a605b6
Anup Barman <183620611+anup-barman@users.noreply.github.com>
2026-06-13 01:41:54 +0600

    update

--- code/String/String Hashing.cpp
+++ code/String/String Hashing.cpp
@@ -1,65 +1,70 @@
-const int mod1 = 911382323, mod2 = 972663749, b1 = 137, b2 = 139;
-const int mxN = 5000010;
-int pow_b1[mxN], pow_b2[mxN], inv_b1[mxN], inv_b2[mxN];
-int binExp(int base, int power, int mod) {
-  int res = 1;
-  while (power) {
-    if (power & 1) res = (1LL * res * base) % mod;
-    base = (1LL * base * base) % mod;
-    power >>= 1;
+const int N = 1000010, MOD = 1e9 + 7;
+const ll P[] = {97, 1000003};
+ll bigMod(ll a, ll e) {
+  if (e == -1) e = MOD - 2;
+  ll ret = 1;
+  while (e) {
+    if (e & 1) ret = ret * a % MOD;
+    a = a * a % MOD, e >>= 1;
   }
-  return res;
+  return ret;
 }
-void pre() {
-  pow_b1[0] = pow_b2[0] = 1;
-  for (int i = 1; i < mxN; i++) {
-    pow_b1[i] = (1LL * pow_b1[i - 1] * b1) % mod1;
-    pow_b2[i] = (1LL * pow_b2[i - 1] * b2) % mod2;
-  }
-  inv_b1[mxN - 1] = binExp(pow_b1[mxN - 1], mod1 - 2, mod1);
-  inv_b2[mxN - 1] = binExp(pow_b2[mxN - 1], mod2 - 2, mod2);
-  for (int i = mxN - 2; i >= 0; i--) {
-    inv_b1[i] = (1LL * inv_b1[i + 1] * b1) % mod1;
-    inv_b2[i] = (1LL * inv_b2[i + 1] * b2) % mod2;
-  }
-}
-vector<pair<int, int>> getPref(string& s) {
-  int qq = s.size();
-  vector<pair<int, int>> hsh(qq);
-  for (int i = 0; i < qq; i++) {
-    if (i == 0) {
-      hsh[i].first = (1LL * s[i] * pow_b1[i]) % mod1;
-      hsh[i].second = (1LL * s[i] * pow_b2[i]) % mod2;
-    } else {
-      hsh[i].first =
-          (hsh[i - 1].first + (1LL * s[i] * pow_b1[i]) % mod1) % mod1;
-      hsh[i].second =
-          (hsh[i - 1].second + (1LL * s[i] * pow_b2[i]) % mod2) % mod2;
+ll pwr[2][N], inv[2][N];
+void initHash() {
+  for (int it = 0; it < 2; ++it) {
+    pwr[it][0] = inv[it][0] = 1;
+    ll INV_P = bigMod(P[it], -1);
+    for (int i = 1; i < N; ++i) {
+      pwr[it][i] = pwr[it][i - 1] * P[it] % MOD;
+      inv[it][i] = inv[it][i - 1] * INV_P % MOD;
     }
   }
-  return hsh;
 }
-pair<int, int> getHash(string& str) {
-  int hsh1 = 0, hsh2 = 0, sz = str.size();
-  for (int i = 0; i < sz; ++i) {
-    hsh1 = (hsh1 + 1LL * str[i] * pow_b1[i] % mod1) % mod1;
+struct RangeHash {
+  vector<ll> h[2], rev[2];
+  RangeHash(const string S, bool revFlag = 0) {
+    for (int it = 0; it < 2; ++it) {
+      h[it].resize(S.size() + 1, 0);
+      for (int i = 0; i < S.size(); ++i) {
+        h[it][i + 1] = (h[it][i] + pwr[it][i + 1] * (S[i] - 'a' + 1)) % MOD;
+      }
+      if (revFlag) {
+        rev[it].resize(S.size() + 1, 0);
+        for (int i = 0; i < S.size(); ++i) {
+          rev[it][i + 1] =
+              (rev[it][i] + inv[it][i + 1] * (S[i] - 'a' + 1)) % MOD;
+        }
+      }
+    }
   }
-  for (int i = 0; i < sz; ++i) {
-    hsh2 = (hsh2 + 1LL * str[i] * pow_b2[i] % mod2) % mod2;
+  inline ll get(int l, int r) {
+    ll one = (h[0][r + 1] - h[0][l]) * inv[0][l + 1] % MOD;
+    ll two = (h[1][r + 1] - h[1][l]) * inv[1][l + 1] % MOD;
+    if (one < 0) one += MOD;
+    if (two < 0) two += MOD;
+    return one << 31 | two;
   }
-  return {hsh1, hsh2};
-}
-pair<int, int> getSub(int l, int r, vector<pair<int, int>>& v) {
-  pair<int, int> q;
-  if (l == 0) {
-    q = {v[r].first, v[r].second};
-  } else {
-    int x = (1LL * ((v[r].first - v[l - 1].first + mod1) % mod1) * inv_b1[l]) %
-            mod1;
-    int y =
-        (1LL * ((v[r].second - v[l - 1].second + mod2) % mod2) * inv_b2[l]) %
-        mod2;
-    q = {x, y};
+  inline ll getReverse(int l, int r) {
+    ll one = (rev[0][r + 1] - rev[0][l]) * pwr[0][r + 1] % MOD;
+    ll two = (rev[1][r + 1] - rev[1][l]) * pwr[1][r + 1] % MOD;
+    if (one < 0) one += MOD;
+    if (two < 0) two += MOD;
+    return one << 31 | two;
   }
-  return q;
+};
+// Returns the length of the longest common
+// prefix of S[l1...r1] and S[l2...r2]
+ll getLCP(RangeHash& rh, int l1, int r1, int l2, int r2) {
+  int len1 = r1 - l1 + 1, len2 = r2 - l2 + 1;
+  int low = 1, high = min(len1, len2), ans = 0;
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if (rh.get(l1, l1 + mid - 1) == rh.get(l2, l2 + mid - 1)) {
+      ans = mid;
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return ans;
 }
